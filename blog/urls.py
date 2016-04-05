e083b13325679d2b4ee4ceb9c7188f7a1012d40d
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-04-05 16:45:17 +0100

    New blogdetail.html and post_detail() view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^blog/$', views.post_list )
+    url(r'^blog/$', views.post_list ),
+    url(r'^blog/(?P<id>\d+)/$',views.post_detail),
     ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,3 +9,7 @@ def test(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now() ).order_by('-published_date')
     return render(request, "blogtests.html",{'posts' : posts } )
+
+def post_detail(request,id):
+    post = get_object_or_404(Post,pk=id)
+    return render(request, "blogdetail.html",{'post' : post} )
