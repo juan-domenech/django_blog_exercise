4d2c00f1084a6363754d30c9ca2a1db663d38c44
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-04-05 15:16:52 +0100

    Static folder and CSS

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from.models import Post
+
+class PostTests(TestCase):
+    def test_str(self):
+        test_title=Post(title='MyLatestBlogPost')
+        self.assertEquals(
+            str(test_title), 'MyLatestBlogPost')
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def test(request):
     return HttpResponse('Test OK!')
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte = timezone.now() ).order_by('-published_date')
+    return render(request, "blogtests.html",{'posts' : posts } )
